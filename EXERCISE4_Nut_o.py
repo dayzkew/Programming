feeb8ef52182608ce916f7dd0c50cff96997d191
KewX <51284548+dayzkew@users.noreply.github.com>
2019-08-03 18:47:12 +0700

    Add files via upload

---
+++ EXERCISE4_Nut_o.py
@@ -0,0 +1,13 @@
+f="Foundation English:"
+FE=70
+g="General Business:"
+GB=60
+i="Introduction to Computer systems:"
+ICS=55
+C="Computer Programming:"
+CR=55.5
+print("-----your Score-----")
+print(f,FE)
+print(g,GB)
+print(i,ICS)
+print(C,CR)
